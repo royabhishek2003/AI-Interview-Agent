6e18cf8f4a80b07d5b3dc93961918dfb643c9e47
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-08-13 18:47:23 +0530

    create a basic client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return(
+     <div className="min-h-screen bg-blue-600 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind is Working! 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
